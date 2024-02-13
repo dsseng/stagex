84160ec5e4f484f633226734c852358be2054273
xyhhx <xyhhx@tuta.io>
2024-02-12 22:09:00 -0500

    feat(package/iputils): start on iputils package

--- src/packages.mk
+++ src/packages.mk
@@ -1093,6 +1093,16 @@ out/sed/index.json: \
 	out/musl/index.json
 	$(call build,sed)
 
+.PHONY: iputils
+iputils: out/iputils/index.json
+out/iputils/index.json:
+	src/tools/iputils/Containerfile \
+	out/binutils/index.json \
+	out/musl/index.json \
+	out/gcc/index.json \
+	out/iputils/index.json
+	$(call build,tools,iputils)
+
 .PHONY: sops
 sops: out/sops/index.json
 out/sops/index.json: \
